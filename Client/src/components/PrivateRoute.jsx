d87c49b85557ff3694eec789c88086e2a7573d94
DeepakGhenyar <deepak.2428cseai2445@kiet.edu>
2026-04-23 16:10:06 +0530

    fix: privateRoute

--- Client/src/components/PrivateRoute.jsx
+++ Client/src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import {  Link, useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
 // Protect routes - only logged-in users can access
